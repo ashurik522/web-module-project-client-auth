2aa8c45b4fade67279e5184edd83f02d3a35b5f9
ashurik522 <ashurik522@gmail.com>
2022-06-22 16:17:00 -0400

    nav bar router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <h1>FRIENDS DATABASE</h1>
+      <ul>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/friends'>Friends List</Link>
+        </li>
+        <li>
+          <Link to='/addfriend'>Add Friend</Link>
+        </li>
+        <li>
+          <Link to='/login'>Logout</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route  path="/login" />
+        <Route  path="/friends" />
+        <Route  path="/addfriend" />
+        <Route  path="/login" />
+      </Switch>
     </div>
   );
 }
